2c4bfb36d43c28abc6e2e13728f81f98809f1a47
gismal <goksu.ismal98@gmail.com>
2026-06-11 22:07:54 +0200

    fix: database setting

--- src/core/database.py
+++ src/core/database.py
@@ -2,14 +2,22 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 from src.config import settings
 
+is_sqlite = str(settings.DATABASE_URL).startswith("sqlite")
+
+engine_kwargs = {
+    "echo": settings.DEBUG,
+    "pool_pre_ping": True
+}
+
+if not is_sqlite:
+    engine_kwargs["pool_size"] = 20
+    engine_kwargs["max_overflow"] = 10
+
 engine = create_async_engine(
     str(settings.DATABASE_URL),
-    echo= settings.DEBUG,
-    pool_size = 20,
-    max_overflow = 10,
-    pool_pre_ping = True
-    )
-
+    **engine_kwargs
+)
+    
 # creates new session each request
 AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush = False)
 
